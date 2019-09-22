860e55a381301ac27cb7ac7b21b35f4629ae4b22
Rain120 <chao1202016@outlook.com>
2019-09-22 14:15:14 +0800

    Create lib.js
    live2d widget libs

---
+++ lib.js
@@ -0,0 +1,25 @@
+const live2d_path = "https://cdn.jsdelivr.net/gh/rain120/live2d-widget/";
+
+$("<link>").attr({ href: live2d_path + "cute-widget.css", rel: "stylesheet" }).appendTo("head");
+
+$.ajax({
+	url: live2d_path + "live2d.min.js",
+	dataType: "script",
+	cache: true
+});
+
+$.ajax({
+	url: live2d_path + "cute-widget-tips.js",
+	dataType: "script",
+	cache: true
+});
+
+$(window).on("load", function() {
+  /** 
+    * function name: initWidget(tips, api)
+    * tips： tips
+    * api: https://live2d.fghrsh.net/api
+    * 参考：https://github.com/fghrsh/live2d_api
+    */
+	initWidget(live2d_path + "cute-widget-tips.json", "https://live2d.fghrsh.net/api");
+});
